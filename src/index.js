366c8cd8f1e171b8034f987ba9662de4a0590a8a
St1ch <st1ch@mail.ru>
2018-03-01 22:11:31 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.number1 = initialValue + '';
+    this.valueOf = () => eval(this.number1);
   }
 
   add(number) {
     // your implementation
+     this.number1 = this.number1 + ' + ' + number
+     return this;
   }
   
   subtract(number) {
     // your implementation
+    this.number1 = this.number1 + ' - ' + number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.number1 =  this.number1 + ' * ' + number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+     this.number1 =  this.number1 + ' / ' + number;
+     return this
   }
 
   pow(number) {
     // your implementation
+    
+    this.number1 = "Math.pow(" + this.number1 + "," + number + ")"
+    return this
   }
 }
 
